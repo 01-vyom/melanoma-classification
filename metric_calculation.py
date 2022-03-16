f48cb6d1cb3775387643c56d9826ea33b113c091
01-vyom <angerstick3@gmail.com>
2022-03-16 17:11:30 -0400

    CHG: Add metric calc with maximization on NPV

--- metric_calculation.py
+++ metric_calculation.py
@@ -2,26 +2,43 @@ import pandas as pd
 import numpy as np
 import glob
 from sklearn.metrics import roc_auc_score, roc_curve, plot_roc_curve, confusion_matrix
+import warnings
+
+# Remove this to see the warning.
+warnings.filterwarnings("ignore")
 
 
 def pretty_per_num(num):
     return round(num * 100, 2)
 
 
-def calc_metrics(pred, target):
+def calc_metrics(pred, target, model_nm):
     auc_score = roc_auc_score(target, pred)
     fpr, tpr, thr = roc_curve(target, pred)
     gmeans = np.sqrt(tpr * (1 - fpr))
-    ix = np.argmax(gmeans)
+    npv = []
+    for i in range(len(thr)):
+        temp_thr = thr[i]
+        pred_thr = [True if p > temp_thr else False for p in pred]
+        tn, fp, fn, tp = confusion_matrix(pred_thr, target).ravel()
+        npv.append(tn / (tn + fn))
+    npv = np.array(npv)
+    # Here whatever we want to maximize, just write that. i.e. np.argmax(npv) will maximize npv, and np.argmax(gmeans) will maximize gmeans.
+    ix = np.argmax(npv)
     bestg = gmeans[ix]
     bestthr = thr[ix]
-    val = "Best Threshold=%f, G-Mean=%.3f" % (bestthr, pretty_per_num(bestg))
+    bestnpv = npv[ix]
+    val = "Best Threshold=%f, G-Mean=%.3f, NPV=%.3f" % (
+        bestthr,
+        pretty_per_num(bestg),
+        pretty_per_num(bestnpv),
+    )
     pred_thr = [True if p > bestthr else False for p in pred]
-    cm1 = confusion_matrix(pred_thr, target)
-    sensitivity = cm1[0, 0] / (cm1[0, 0] + cm1[0, 1])
-    specificity = cm1[1, 1] / (cm1[1, 0] + cm1[1, 1])
+    tn, fp, fn, tp = confusion_matrix(pred_thr, target).ravel()
+    sensitivity = tp / (tp + fn)
+    specificity = tn / (tn + fp)
     print(
-        "Following are the metrics: AUC_Score:",
+        "\nFollowing are the metrics for " + model_nm + ": AUC_Score:",
         pretty_per_num(auc_score),
         val,
         "Sensitivity:",
@@ -50,18 +67,8 @@ def calculate_metric_ensemble(path):
             samessies[tuple(targ)] = [nm]
         preds[nm] = list(data["pred"].values)
         calc_metrics(
-            list(data["pred"]), list((data["target"] == data["mel_index"].iloc[0]))
+            list(data["pred"]), list((data["target"] == data["mel_index"].iloc[0])), nm
         )
-
-    targets = pd.DataFrame(targets)
-    preds = pd.DataFrame(preds)
-
-    col_nms = list(samessies.values())[0]
-    target = list(samessies.keys())[0]
-
-    print("Following Models were selected for the ensemble: ", col_nms)
-
-    calc_metrics(list(preds[col_nms].mean(axis=1).values), target)
 
 
 def main():
