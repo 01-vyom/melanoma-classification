9070506aaeeb1c4e2c7c7d6f7c7c92c61d14244f
01-vyom <angerstick3@gmail.com>
2022-04-10 20:09:31 -0400

    ENH: Add script for EffcientnetB5 and Resnet101

---
+++ efficientnet.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=EfficientNet
+#SBATCH --output=vasp.out
+#SBATCH --error=vasp.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=v.pathak@ufl.edu
+#SBATCH --nodes=1                    
+#SBATCH --ntasks=1                   
+#SBATCH --cpus-per-task=10            
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mem=100gb
+#SBATCH --time=48:00:00
+
+echo "Date      = $(date)"
+echo "host      = $(hostname -s)"
+echo "Directory = $(pwd)"
+
+module purge
+ml pytorch/1.8.1
+
+T1=$(date +%s)
+python BYOL_SIIM-ISIC_Efficientnetb5.py
+T2=$(date +%s)
+
+ELAPSED=$((T2 - T1))
+echo "Elapsed Time = $ELAPSED"
+
